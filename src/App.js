acd25f23ef422fbfd17b74d0430b7345e6c07f53
GYANANKUR <ankurdey429@gmail.com>
2021-11-02 23:28:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Row from "./Row.js";
+import requests from './requests.js';
+import Banner from './Banner.js';
+import './App.css'
+import Nav from './Nav.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav></Nav>
+     <Banner></Banner>
+      <Row 
+      title="NETFLIX ORIGINALS" 
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
+
     </div>
+    
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,23 @@
+import  { useEffect, useState } from 'react';
+import './Nav.css'
+function Nav() {
+    const[show , handleShow] = useState(false);
+    useEffect(() =>{
+        window.addEventListener('scroll', () =>{
+            if(window.scrollY > 100){
+                handleShow(true);
+            }else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener('scroll');
+        };
+    }, []);
+    return(
+        <div className={`nav ${show && "nav__black"}`}>
+            <img className="nav__logo"src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            alt="Neflix-Logo"></img>
+        </div>
+    );
+}
+
+export default Nav
